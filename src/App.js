412a3d30026693e15b7cacc3ba061af2bcde295e
Zoran Nikolov <zoran902@gmail.com>
2023-08-26 22:30:22 +0300

    added chakra library and firebase hosting to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import "./App.css";
+import {
+	Accordion,
+	AccordionItem,
+	AccordionButton,
+	AccordionPanel,
+	AccordionIcon,
+	Box,
+} from "@chakra-ui/react";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>this is test</p>
-			</header>
-		</div>
+		<Accordion>
+			<AccordionItem>
+				<h2>
+					<AccordionButton>
+						<Box as="span" flex="1" textAlign="left">
+							Section 1 title
+						</Box>
+						<AccordionIcon />
+					</AccordionButton>
+				</h2>
+				<AccordionPanel pb={4}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+					do eiusmod tempor incididunt ut labore et dolore magna
+					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</AccordionPanel>
+			</AccordionItem>
+
+			<AccordionItem>
+				<h2>
+					<AccordionButton>
+						<Box as="span" flex="1" textAlign="left">
+							Section 2 title
+						</Box>
+						<AccordionIcon />
+					</AccordionButton>
+				</h2>
+				<AccordionPanel pb={4}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+					do eiusmod tempor incididunt ut labore et dolore magna
+					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</AccordionPanel>
+			</AccordionItem>
+		</Accordion>
 	);
 }
 
