7da7517115008bf7562d92d60a839878036d2b0f
ZoranNikolov <zoran902@gmail.com>
2023-12-19 14:06:52 +0200

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,21 @@ import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 const Footer = () => {
 	return (
 		<footer className="width-100 bg-dark text-white">
-			<Container>
+			<Container className="pl-0 pr-0 width-60">
 				<Row className="justify-content-around align-items-center">
-					<Col className="justify-content-start mt-3">
-						<p>&copy; 2023 ZODI90 Ltd. All rights reserved.</p>
+					<Col className="justify-content-center mt-3">
+						<Row className="justify-content-center">
+							<Col>
+						<p className="text-align-center">&copy; 2023 ZODI90 Ltd. All rights reserved.</p>
+							</Col>
+						</Row>
 					</Col>
-					<Col className="mt-3">
+					<Col className="">
 						<div className="d-flex justify-content-end">
-							<a href="#" className="text-white me-2">
+							<a href="#" className="text-white me-4">
 								<FaFacebook fontSize="20px" />
 							</a>
-							<a href="#" className="text-white me-2">
+							<a href="#" className="text-white me-4">
 								<FaTwitter fontSize="20px" />
 							</a>
 							<a href="#" className="text-white">
